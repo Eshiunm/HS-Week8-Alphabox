3b0f7c27e5d293b097def1c2528554b780e5da46
Julie <juliy0214561@gmail.com>
2023-08-31 14:37:20 +0800

    [modify&upload] 修改使用分享區塊卡片、上傳最新消息icon

--- assets/js/_share_use.js
+++ assets/js/_share_use.js
@@ -7,11 +7,17 @@ const swiper = new Swiper('.swiper', {
     // slide 間距
     spaceBetween: 24,
     slidesPerView: 3,
-    // Scrollbar
+    // Scrollbar 滑鼠中鍵滾輪可滑動
     scrollbar: {
        el: '.swiper-scrollbar',
        draggable: true,
     },
     // 使 swiper 可以使用滑鼠滾輪捲動
     mousewheel: true,
+   //  slidesPerView: "auto",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
   });
+ 
