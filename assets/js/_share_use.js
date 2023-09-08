b1d4593140aa784ca43ce3acec6d1544e856c22e
Julie <juliy0214561@gmail.com>
2023-09-09 00:22:25 +0800

    [fix & modify] 修副產品頁有x軸問題、上傳產品頁的RWD

--- assets/js/_share_use.js
+++ assets/js/_share_use.js
@@ -10,26 +10,14 @@ const swiper = new Swiper('.swiper_share_use', {
         // slide 間距
         spaceBetween: 24,
         slidesPerView: 3,
-        direction: "horizontal",
-        // Scrollbar 滑鼠中鍵滾輪可滑動
-        //if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-          draggable: true,
-        },
-      },
-      372:{
-        spaceBetween: 24,
-        slidesPerView: 1,
-        direction: "vertical",
-        scrollbar: {
-          el: '.swiper-scrollbar',
-          draggable: false,
-        },
-        // grid: {
-        //   columns: 4,
-        // }
+        direction: "horizontal"
       }
+    },
+    // Scrollbar 滑鼠中鍵滾輪可滑動
+    //if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
     },
     // 使 swiper 可以使用滑鼠滾輪捲動
     mousewheel: true,
